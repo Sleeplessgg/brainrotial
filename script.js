b85b1f43ba7ab453076a3995e5c6a078f12747c9
Sleeplessgg <161692320+Sleeplessgg@users.noreply.github.com>
2026-01-30 10:25:50 +0000

    removed trailing hypen in Returned string

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  
 var brainrotData = require('./brainrots.json');
-console.log(calculateBrainrotString(10));
+console.log(calculateBrainrotString(68));
 
 function calculateBrainrotString(input) {
     let number = input;
@@ -8,7 +8,8 @@ function calculateBrainrotString(input) {
     do{
         result =  brainrotData[number % 67] + "-" + result;
         number = Math.trunc(number / 67);
-    } while((number/67) >= 1);
+    } while(number >= 1);
 
+    result = result.slice(0, -1);
     return result;
 }
